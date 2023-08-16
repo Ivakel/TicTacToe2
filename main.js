3f5ec09425d54ef94fea13998290dde1ab2b619b
Ivakel <ivakeledlabom88@gmail.com>
2023-08-16 22:06:42 +0200

    another push

---
+++ main.js
@@ -0,0 +1,125 @@
+let game = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+
+const whichPlayer = { 0: "O", 1: "X" };
+
+const coOrds = {
+  ".s11": [0, 0],
+  ".s12": [0, 1],
+  ".s13": [0, 3],
+  ".s21": [0, 0],
+  ".s22": [0, 0],
+  ".s23": [0, 0],
+  ".s31": [0, 0],
+  ".s32": [0, 0],
+  ".s33": [0, 0],
+};
+
+const playerCross = 1;
+const playerCircle = 0;
+const wins = new Set(["ooo", "xxx"]);
+const announcer = document.querySelector(".announcer");
+
+let turn = false;
+
+let gameOn = true;
+
+//Joins a row to a string
+function join(row) {
+  let r = ""; // string to append each row element
+  for (let i = 0; i < 3; i++) {
+    r += row[i];
+  }
+  return r;
+}
+
+function winner(player) {
+  let w = "";
+  if (player === 1) {
+    w = "X";
+  } else {
+    w = "O";
+  }
+  announcer.innerHTML = w + " HAS WON!!!";
+}
+
+function getCol() {
+  let cols = [];
+
+  for (let i = 0; i < 3; i++) {
+    let str = "";
+    for (let j = 0; j < 3; j++) {
+      str += game[j][i];
+    }
+    cols.push(str);
+  }
+  return cols;
+}
+
+function gameOver() {
+  //check for each row
+  for (let i = 0; i < 3; i++) {
+    let str = join(game[i]);
+
+    if (wins.has(str)) {
+      //check if someone won
+      if (str === "xxx") {
+        winner(1);
+        return;
+      }
+      winner(0);
+      return;
+    }
+  }
+  //check for each column and crosses
+  let cols = getCols();
+  let n = 0;
+  let str = "";
+
+  while (n < 3) {
+    str += game[n][n];
+    n += 1;
+  }
+  cols.push(str);
+
+  n = 0;
+  str = "";
+
+  while (n < 3) {
+    str += game[n][2 - n];
+    n += 1;
+  }
+  cols.push(str);
+
+  for (let i = 0; i < 3; i++) {
+    let str = cols[i];
+    if (wins.has(str)) {
+      if (str === "xxx") {
+        winner(1);
+      }
+      winner(0);
+      return;
+    }
+  }
+}
+
+function myFunction(el) {
+  const btn = document.querySelector("." + el.className);
+  //btn.innerHTML = '<img src="img/circle.png" alt="" />';
+  if (btn.innerHTML === "") {
+    if (turn) {
+      [a, b] = coOrds["." + el.className];
+      game[(a, b)] = "o";
+      btn.innerHTML = '<img src="img/circle.png" alt="" />';
+    } else {
+      [a, b] = coOrds["." + el.className];
+      game[(a, b)] = "x";
+      btn.innerHTML = '<img src="img/x-icon.png" alt="" />';
+    }
+    turn = !turn;
+    gameOver();
+  }
+}
